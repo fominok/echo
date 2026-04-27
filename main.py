3536d5c794e69f2324625a7a8c1a29efc1cd1a9e
fominok <fominok@hotmail.com>
2026-04-27 07:08:30 +0000

    yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Optional
+from typing import Optional, Any
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -12,3 +12,16 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/echo")
+async def echo(request: Request):
+    content_type = request.headers.get("content-type", "")
+    
+    if "application/json" not in content_type:
+        return {"error": "expected a json"}
+    
+    try:
+        body = await request.json()
+        return {"echo": body}
+    except Exception:
+        return {"error": "expected a json"}
